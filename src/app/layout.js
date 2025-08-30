14f0a85f1d29fe22221bff45c5d97369de7af06a
litvinenko <litvinenko_n.v@mail.ru>
2025-08-30 19:37:52 +0300

    dd

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,14 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800&display=swap" 
+          rel="stylesheet" 
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
